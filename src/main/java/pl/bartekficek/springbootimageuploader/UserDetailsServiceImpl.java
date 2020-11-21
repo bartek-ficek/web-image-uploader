bf4914cd9f0b91fe5436896dd94852fe127a6d26
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-11-21 23:52:41 +0100

    1:10

--- src/main/java/pl/bartekficek/springbootimageuploader/UserDetailsServiceImpl.java
+++ src/main/java/pl/bartekficek/springbootimageuploader/UserDetailsServiceImpl.java
@@ -1,24 +1,38 @@
 package pl.bartekficek.springbootimageuploader;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import pl.bartekficek.springbootimageuploader.model.AppUser;
 import pl.bartekficek.springbootimageuploader.repo.AppUserRepo;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private AppUserRepo appUserRepo;
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserDetailsServiceImpl(AppUserRepo appUserRepo) {
+    public UserDetailsServiceImpl(AppUserRepo appUserRepo, PasswordEncoder passwordEncoder) {
         this.appUserRepo = appUserRepo;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         return appUserRepo.findByUsername(s);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void get() {
+        AppUser appUser = new AppUser("Jan",
+                passwordEncoder.encode("Nowak"),
+                "User");
+        appUserRepo.save(appUser);
     }
 }
